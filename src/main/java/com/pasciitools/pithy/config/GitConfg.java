4beefe2d988543f5990f3748c67178d50f2933e4
Pascal LD <pasdeignan@gmail.com>
2022-12-16 11:40:41 -0800

    externalized logo image and added blog-repo as a resource path

--- src/main/java/com/pasciitools/pithy/config/GitConfg.java
+++ src/main/java/com/pasciitools/pithy/config/GitConfg.java
@@ -5,10 +5,14 @@ import lombok.Data;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,7 +21,9 @@ import java.io.IOException;
 @ConfigurationProperties(prefix = "git")
 @Validated
 @Data
-public class GitConfg {
+public class GitConfg implements WebMvcConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(GitConfg.class);
 
     @NotEmpty
     private String repoPath;
@@ -44,4 +50,13 @@ public class GitConfg {
         return new UsernamePasswordCredentialsProvider(user, password);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        repoPath = repoPath.endsWith(File.separator) ? repoPath : repoPath + File.separator;
+        registry.
+                addResourceHandler("/blog-repo/**").
+                addResourceLocations("file:" + repoPath).
+                setCachePeriod(3600).
+                resourceChain(true);
+    }
 }
